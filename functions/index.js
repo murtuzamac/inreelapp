4423431ffab61be6524013d437ddecfed5e46b0e
Murtuza Chakkiwala <murtuza.chakkiwala@CIGNEXDATAMATICS.COM>
2018-02-27 20:01:31 +0530

    coments

--- functions/index.js
+++ functions/index.js
@@ -9,6 +9,7 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 const reviewFeedPageSize = 5;
+const commentPageSize = 9;
 
 exports.reviews = functions.https.onRequest((req, res) => {
   var userId = req.query.userId;
@@ -49,6 +50,50 @@ exports.reviews = functions.https.onRequest((req, res) => {
         result: [],
         status: 'success',
         startKey: null
+      }
+      res.send(result);
+    }
+  });
+});
+
+exports.comments = functions.https.onRequest((req, res) => {
+  var startKey = req.query.startKey;
+  var refPath = req.query.refPath;
+  var dbRefPath = admin.database().ref(refPath);
+  var getComment;
+  var query = startKey ? dbRefPath.orderByKey().limitToLast(commentPageSize).endAt(startKey) : dbRefPath.orderByKey().limitToLast(commentPageSize);
+  // var query = admin.database().ref("/users/" + userId + "/feed").orderByKey().limitToLast(reviewFeedPageSize);
+  query.once("value", function (comments) {
+    if (comments.exists()) {
+      var commentKeysArr = Object.keys(comments.val()),
+        commentArr = [];
+      commentKeysArr.reverse();
+      getComment = (commentId, index) => {
+        getSingleComment(commentId, (commentJSON) => {
+          var len = (commentKeysArr.length < commentPageSize) ? 1 : 2; //to handle situation when thre are less records than the page size in the batch
+          var _index = index + 1;
+          commentArr.push(commentJSON)
+          //condition to continue the recursion or not
+          if (index < commentKeysArr.length - len) {
+            getComment(commentKeysArr[_index], _index);
+          }
+          else {
+            var result = {
+              result: commentArr,
+              status: 'success',
+              startKey: commentKeysArr[index + 1] ? commentKeysArr[index + 1] : null
+            }
+            res.send(result);
+          }
+        })
+      }
+      getComment(commentKeysArr[0], 0);
+    }
+    else {
+      var result = {
+        result: [],
+        status: 'success',
+        startKey: null,
       }
       res.send(result);
     }
@@ -109,5 +154,30 @@ getSingleReviewDetails = (reviewId, userId, callback) => {
       })
     })
 
+  })
+}
+
+getSingleComment = (commentId, callback) => {
+  var commentRef = admin.database().ref("comments/" + commentId);
+  commentRef.once("value", function (commentSnapshot) {
+    var _comment = commentSnapshot.val();
+
+    //json design for a single comment 
+    var commentJSON = {
+      commentId: commentId,
+      userId: _comment.commentuserid,
+      userName: '',
+      userAvatar: '',
+      commentTime: _comment.commenttime,
+      commentText: _comment.commenttext
+    }
+
+    admin.database().ref("users/" + _comment.commentuserid).once('value', function (userSnapshot) {
+      var user = userSnapshot.val();
+      commentJSON.userName = user.username;
+      commentJSON.userAvatar = user.useravatar;
+
+      callback(commentJSON)
+    })
   })
 }
